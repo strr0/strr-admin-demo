456f0c6c535d796bbddd3a7668ccfbf4061207ee
strr0 <38580170+strr0@users.noreply.github.com>
2023-07-15 22:32:09 +0800

    feat:strr-admin-cloud-demo

---
+++ strr-admin-feign/src/main/java/com/strr/FeignServerApplication.java
@@ -0,0 +1,13 @@
+package com.strr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class FeignServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FeignServerApplication.class, args);
+    }
+}
